31487047a9e1946a72386e4ef0aaee2e919c711a
yavu <nisiba0330@gmail.com>
2023-09-25 13:59:45 +0900

    テーマやfaviconなど

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,139 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
+import { ReactNode } from 'react';
 import './App.css';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Divider, FormControl, MenuItem, Paper, Select, SelectChangeEvent, SxProps, TextField, Typography } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import { DarkTheme } from './theme/dark';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(): JSX.Element {
+    return (
+        <>
+            <header>
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+            </header>
+            <body>
+                <ThemeProvider theme={DarkTheme}>
+                    <CssBaseline />
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="center"
+                        flexWrap="nowrap"
+                        alignItems="flex-start"
+                        overflow="hidden"
+                        sx={{
+                            minWidth: DarkTheme.spacing(26)
+                        }}
+                    >
+                        <Paper
+                            elevation={2}
+                            sx={{
+                                height: "auto",
+                                width: DarkTheme.spacing(47),
+                                padding: DarkTheme.spacing(1),
+                                paddingBottom: "0",
+                                margin: DarkTheme.spacing(1),
+                                overflowX: "hidden",
+                                overflowY: "auto",
+                                "@media screen and (max-width:800px)": {
+                                    width: DarkTheme.spacing(24)
+                                }
+                            }}
+                        >
+                            <Typography
+                                variant="h5"
+                            >
+                                ReflecMino
+                            </Typography>
+                            <Divider
+                                sx={{
+                                    marginTop: DarkTheme.spacing(0.5),
+                                    marginBottom: DarkTheme.spacing(1)
+                                }}
+                            />
+                            <Grid
+                                container
+                                direction="row"
+                                justifyContent="center"
+                                alignItems="flex-start"
+                                gap={DarkTheme.spacing(1)}
+                            >
+                                <PropertyWrapper
+                                    width={DarkTheme.spacing(22)}
+                                    height={DarkTheme.spacing(22)}
+                                >
+                                    <Canvas
+                                        width={DarkTheme.spacing(20)}
+                                        height={DarkTheme.spacing(20)}
+                                    />
+                                </PropertyWrapper>
+                                <PropertyWrapper
+                                    width={DarkTheme.spacing(22)}
+                                    height={DarkTheme.spacing(22)}
+                                >
+                                    <Canvas
+                                        width={DarkTheme.spacing(20)}
+                                        height={DarkTheme.spacing(20)}
+                                    />
+                                </PropertyWrapper>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                </ThemeProvider>
+            </body>
+        </>
+    )
 }
 
-export default App;
+type PropertyWrapper = {
+    children: ReactNode;
+    width: string;
+    height: string;
+};
+
+function PropertyWrapper({ width, height, children }: PropertyWrapper): JSX.Element {
+    return (
+        <Paper
+            elevation={6}
+            sx={{
+                width: width,
+                height: height,
+                padding: DarkTheme.spacing(1),
+                marginBottom: DarkTheme.spacing(1)
+            }}
+        >
+            {children}
+        </Paper>
+    )
+}
+
+type Canvas = {
+    width: string;
+    height: string;
+};
+
+function Canvas({ width: width, height: height }: Canvas) {
+    const canvas_ref = useRef(null);
+
+    const get_context = (): CanvasRenderingContext2D => {
+        const canvas: any = canvas_ref.current;
+        return canvas.getContext('2d');
+    };
+
+    useEffect(() => {
+        const ctx: CanvasRenderingContext2D = get_context();
+        ctx.fillRect(0, 0, 100, 100);
+        ctx.save();
+    })
+
+    return (
+        <>
+            <canvas ref={canvas_ref} width={width} height={height} />
+        </>
+    );
+}
---
+++ src/theme/dark.tsx
@@ -0,0 +1,75 @@
+import { createTheme } from '@mui/material/styles';
+
+export const DarkTheme = createTheme({
+    typography: {
+        fontFamily: "Share Tech Mono",
+        button: {
+            fontWeight: 600,
+        },
+    },
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#00b0ff",
+            contrastText: "#2a3648",
+        },
+        secondary: {
+            main: "#ef6c00",
+        },
+        background: {
+            default: "#101d31",
+            paper: "#101d31",
+        },
+        error: {
+            main: "#ff1744",
+        },
+    },
+    spacing: 16,
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: `
+            ::-webkit-scrollbar {
+                width: 8px;
+                height: 8px;
+            }
+            
+            ::-webkit-scrollbar-track {
+                background: transparent;
+            }
+            
+            ::-webkit-scrollbar-thumb {
+                background: rgba(30, 160, 255, 0.3);
+                border-radius: 8px;
+                opacity: 0.1;
+            }
+            @font-face {
+              font-family: 'Share Tech Mono';
+              font-style: normal;
+              font-weight: 400;
+              font-display: swap;
+              src: url(https://fonts.gstatic.com/s/sharetechmono/v15/J7aHnp1uDWRBEqV98dVQztYldFcLowEF.woff2) format('woff2');
+              unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+            }
+            `
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: ({ ownerState }) => ({
+                    ...(ownerState.size === "medium" && {
+                        boxSizing: "border-box",
+                        height: "40px",
+                        paddingTop: "8px",
+                    })
+                })
+            }
+        },
+        MuiToggleButtonGroup: {
+            styleOverrides: {
+                root: {
+                    boxSizing: "border-box",
+                    height: "40px"
+                }
+            }
+        }
+    }
+});
