a46f01da794c4d58985433a9a594cc0be9cd413b
yavu <nisiba0330@gmail.com>
2023-09-29 09:49:31 +0900

    三項演算子 改行できるじゃん

--- src/puzzle/generate.ts
+++ src/puzzle/generate.ts
@@ -44,29 +44,27 @@ export function generate(seed: number) {
             let x = laser[i].x;
             let y = laser[i].y;
             for (let mirror = 1; mirror > 0; mirror--) {
-                const pick = {
-                    "0": { "0": board[3][x - move[1]], "1": board.map((a) => a[x]), "2": board[3][x + move[1]] },
-                    "1": { "0": board[y - move[0]][3], "1": board[y], "2": board[y + move[0]][3] }
-                }[move[0] === 0 ? 0 : 1];
+                const pick = move[0] === 0
+                    ? { "0": board[3][x - move[1]], "1": board.map((a) => a[x]), "2": board[3][x + move[1]] }
+                    : { "0": board[y - move[0]][3], "1": board[y], "2": board[y + move[0]][3] }
                 const sort = move[0] + move[1] < 0 ? [...pick[1]].reverse().map(e => e.replace(/u002F/g, "w").replace(/u005C/g, "/").replace(/w/g, "\\")) : [...pick[1]];
-                const trim_forward = {
-                    "0": move[1] === 1 ? [...sort].slice(y + 1, sort.length - 1) : [...sort].slice(sort.length - y, [...sort].length - 1),
-                    "1": move[0] === 1 ? [...sort].slice(x + 1, sort.length - 1) : [...sort].slice(sort.length - x, [...sort].length - 1)
-                }[move[0] === 0 ? 0 : 1];
+                const trim_forward = move[0] === 0
+                    ? move[1] === 1 ? [...sort].slice(y + 1, sort.length - 1) : [...sort].slice(sort.length - y, [...sort].length - 1)
+                    : move[0] === 1 ? [...sort].slice(x + 1, sort.length - 1) : [...sort].slice(sort.length - x, [...sort].length - 1)
                 const trim_r_mirror = [...trim_forward].slice(0, sort.includes("/") ? sort.indexOf("/") : undefined);
                 const trim_l_mirror = [...trim_r_mirror].slice(0, sort.includes("\\") ? sort.indexOf("\\") : undefined);
                 const trim_deadend_mirror = ([...trim_l_mirror][trim_l_mirror.length - 1] === "/" && pick[0] === "#") || ([...trim_l_mirror][trim_l_mirror.length - 1] === "\\" && pick[2] === "#") ? [...trim_l_mirror].slice(0, trim_l_mirror.length - 1) : [...trim_l_mirror];
                 const range = [...trim_deadend_mirror];
                 console.log(`${x},${y}`);
-                console.log(`${pick[0]}\n${range.join()}\n${pick[2]}`);
-                //console.log(`${range.join()}`);
+                //console.log(`${pick[0]}\n${range.join()}\n${pick[2]}`);
+                console.log(`${range.join()}`);
             }
         }
         return board;
     }
     const board = random_mirror(empty_board);
     console.log(`${board[0].join("")}\n${board[1].join("")}\n${board[2].join("")}\n${board[3].join("")}\n${board[4].join("")}\n${board[5].join("")}\n${board[6].join("")}`);
-    console.log(laser);
+    //console.log(laser);
     console.log("======================");
 }
 
