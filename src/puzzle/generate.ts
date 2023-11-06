675f0972cb3dfe55c6baa63d0956c9d2df412b3e
yavu <nisiba0330@gmail.com>
2023-11-06 15:12:10 +0900

    再帰でランダムに4つ置く処理を書いた

--- src/puzzle/generate.ts
+++ src/puzzle/generate.ts
@@ -24,10 +24,10 @@ export function generate(seed: number) {
 
 
 
-    type LaserDraw = [board: string[][], x: number, y: number, move: Move, mirror: number];
+    type DrawLaser = [board: string[][], x: number, y: number, move: Move, mirror: number];
     // ミラーを必要数置きつつレーザーを描画する関数
     const draw_random_laser = (board: string[][], laser: { mirror: number, x: number, y: number, move: Move }) => {
-        const move_laser = (data: LaserDraw[]) => {
+        const move_laser = (data: DrawLaser[]) => {
             const current = data[data.length - 1];
             const board = current[0];
             const x = current[1];
@@ -84,18 +84,18 @@ export function generate(seed: number) {
                 ? range[rnd.next_int(0, range.length)]
                 : range[range.length - 1];
             // 1マス進んでboardに書き込む
-            const draw_laser = (data: LaserDraw) => {
+            const draw_laser = (data: DrawLaser) => {
                 const x = data[1] + data[3][0];
                 const y = data[2] + data[3][1];
                 const board = replace_2d_array(data[0], x, y, "￭");
-                const new_data: LaserDraw = [board, x, y, data[3], data[4]];
+                const new_data: DrawLaser = [board, x, y, data[3], data[4]];
                 return new_data;
             }
-            const lined_data: LaserDraw = random_range > 0
+            const lined_data: DrawLaser = random_range > 0
                 ? compose_n(random_range, draw_laser)(current)
                 : current;
             // ミラー設置関数
-            const set_mirror = (data: LaserDraw) => {
+            const set_mirror = (data: DrawLaser) => {
                 const x = data[1] + data[3][0];
                 const y = data[2] + data[3][1];
                 const random_turn = rnd.next_bool();
@@ -112,11 +112,11 @@ export function generate(seed: number) {
                         return structuredClone(data[0]);
                     }
                 })();
-                const new_data: LaserDraw = [board, data[1], data[2], data[3], mirror];
+                const new_data: DrawLaser = [board, data[1], data[2], data[3], mirror];
                 return new_data;
             }
             // 反射関数
-            const reflection = (data: LaserDraw) => {
+            const reflection = (data: DrawLaser) => {
                 const x = data[1] + data[3][0];
                 const y = data[2] + data[3][1];
                 const turn_move = (direction: boolean, move: Move): Move => {
@@ -148,11 +148,11 @@ export function generate(seed: number) {
                         return data[3];
                     }
                 })();
-                const new_data: LaserDraw = [data[0], x, y, move, data[4]];
+                const new_data: DrawLaser = [data[0], x, y, move, data[4]];
                 return new_data;
             }
             // 返すデータを作成
-            const new_data: LaserDraw[] = (() => {
+            const new_data: DrawLaser[] = (() => {
                 if (mirror > 0) {
                     const result = reflection(set_mirror(lined_data));
                     if (range.length !== 0) {
@@ -166,7 +166,7 @@ export function generate(seed: number) {
                     }
                 }
                 else {
-                    const result: LaserDraw = (() => {
+                    const result: DrawLaser = (() => {
                         const data = reflection(lined_data);
                         const board = data[0][y][x] === " "
                             ? replace_2d_array(data[0], data[1] - data[3][0], data[2] - data[3][1], "￭")
@@ -179,7 +179,7 @@ export function generate(seed: number) {
             return new_data;
         }
         // レーザー必要数ミラーを接地し壁に衝突するまで処理
-        const initial: LaserDraw[] = [[board, laser.x, laser.y, laser.move, laser.mirror]];
+        const initial: DrawLaser[] = [[board, laser.x, laser.y, laser.move, laser.mirror]];
         const new_data = while_f(initial, s => {
             const result = move_laser(s);
             const current = result[result.length - 1];
@@ -190,115 +190,119 @@ export function generate(seed: number) {
     }
 
     // レーザーを2本描画したボードを返す関数
-    const draw_2_laser = (): string[][] => {
-        const first_draw = (): LaserDraw => {
+    const draw_two_laser = (): string[][] => {
+        const draw_one = (): DrawLaser => {
             const data = draw_random_laser(empty_board, laser[0]);
             if (data[1] !== laser[1].x || data[2] !== laser[1].y) {
                 return data;
             }
             else {
-                return first_draw();
+                return draw_one();
                 // レーザーの開始地点同士が繋がっていないデータが出るまで再帰
             }
         };
-        const first_drawn_data = first_draw();
-        const second_drawn_data = draw_random_laser(first_drawn_data[0], laser[1]);
-        const laser_cell_count = [...second_drawn_data[0]].join().replace(/[^\\/￭]/g, "").length;
+        const draw_1_data = draw_one();
+        const draw_2_data = draw_random_laser(draw_1_data[0], laser[1]);
+        const laser_cell_count = [...draw_2_data[0]].join().replace(/[^\\/￭]/g, "").length;
         if (laser_cell_count > 11) {
             const s_drawn_board = (() => {
-                const first_drawn = replace_2d_array(second_drawn_data[0], laser[0].x, laser[0].y, "s");
-                const second_drawn = replace_2d_array(first_drawn, laser[1].x, laser[1].y, "s");
-                return second_drawn;
+                const draw_1 = replace_2d_array(draw_2_data[0], laser[0].x, laser[0].y, "s");
+                const draw_2 = replace_2d_array(draw_1, laser[1].x, laser[1].y, "s");
+                return draw_2;
             })();
             const all_drawn_board = (() => {
-                const first_drawn = replace_2d_array(s_drawn_board, first_drawn_data[1], first_drawn_data[2], "e");
-                const second_drawn = replace_2d_array(first_drawn, second_drawn_data[1], second_drawn_data[2], "e");
-                return second_drawn;
+                const draw_1 = replace_2d_array(s_drawn_board, draw_1_data[1], draw_1_data[2], "e");
+                const draw_2 = replace_2d_array(draw_1, draw_2_data[1], draw_2_data[2], "e");
+                return draw_2;
             })();
             return all_drawn_board;
         }
         else {
-            return draw_2_laser();
+            return draw_two_laser();
             // レーザーの通過マスが12以上のデータが出るまで再帰
         }
     };
-    const laser_drawn_board = draw_2_laser();
-    console.log([...laser_drawn_board].join("\n").replace(/,/g, " "));
-    // console.log([...laser_drawn_board].join("\n").replace(/[^\\/￭\n,]/g, "#").replace(/[,￭]/g, " ").replace(/[\\/]/g, "•"));
+
+    const laser_drawn_board = draw_two_laser();
+    console.log([...laser_drawn_board].join("\n").replace(/,/g, "  "));
+
     const laser_cell_count = [...laser_drawn_board].join().replace(/[^\\/￭]/g, "").length;
     console.log(laser_cell_count);
 
-    const mirror_cell: { x: number, y: number }[] = [...laser_drawn_board].map((y, y_index) => y.map((x, x_index) => x === "\\" || x === "/" ? { x: x_index, y: y_index } : { x: -1, y: -1 }).filter(e => e.x !== -1)).flat();
-    console.log(JSON.stringify([...mirror_cell]));
+    const laser_cells: { x: number, y: number }[] = [...laser_drawn_board].map((y, y_index) => y.map((x, x_index) => x === "\\" || x === "/" || x === "￭" ? { x: x_index, y: y_index } : { x: -1, y: -1 }).filter(e => e.x !== -1)).flat();
 
-    const place_random_mino = (data: [board: string[][], x: number, y: number]): [string[][], boolean] => {
-        const x = data[1];
-        const y = data[2];
-        if (data[0][y][x] === "/" || data[0][y][x] === "\\") {
-            const placeable_cell = [
-                [" ", " ", data[0][y - 2]?.[x] ?? "#", " ", " "],
-                [" ", data[0][y - 1]?.[x - 1] ?? "#", data[0][y - 1]?.[x] ?? "#", data[0][y - 1]?.[x + 1] ?? "#", " "],
-                [data[0][y]?.[x - 2] ?? "#", data[0][y]?.[x - 1] ?? "#", "x", data[0][y]?.[x + 1] ?? "#", data[0][y]?.[x + 2] ?? "#"],
-                [" ", data[0][y + 1]?.[x - 1] ?? "#", data[0][y + 1]?.[x] ?? "#", data[0][y + 1]?.[x + 1] ?? "#", " "],
-                [" ", " ", data[0][y + 2]?.[x] ?? "#", " ", " "]
-            ].map(y => y.map(x => x.replace(/[\\/]/g, "￭")));
-            //console.log([...placeable_cell].join("\n").replace(/,/g, " "));
+    type PlaceMino = [board: string[][], laser_cells: { x: number, y: number }[], placed_mino: number]
+    const place_random_mino = (data: PlaceMino): PlaceMino => {
+        const board = data[0];
+        const random_pos = data[1][rnd.next_int(0, data[1].length)];
+        const x = random_pos.x;
+        const y = random_pos.y;
+        const placeable_cell = [
+            [" ", " ", board[y - 2]?.[x] ?? "#", " ", " "],
+            [" ", board[y - 1]?.[x - 1] ?? "#", board[y - 1]?.[x] ?? "#", board[y - 1]?.[x + 1] ?? "#", " "],
+            [board[y]?.[x - 2] ?? "#", board[y]?.[x - 1] ?? "#", "x", board[y]?.[x + 1] ?? "#", board[y]?.[x + 2] ?? "#"],
+            [" ", board[y + 1]?.[x - 1] ?? "#", board[y + 1]?.[x] ?? "#", board[y + 1]?.[x + 1] ?? "#", " "],
+            [" ", " ", board[y + 2]?.[x] ?? "#", " ", " "]
+        ].map(y => y.map(x => x.replace(/[\\/]/g, "￭")));
+        // console.log([...placeable_cell].join("\n").replace(/,/g, " "));
 
-            // 絶対もっといい方法ある
-            const placeable_mino = [
-                placeable_cell[0][2] === "￭" && placeable_cell[1][2] === "￭" ? 0 : -1,
-                placeable_cell[1][2] === "￭" && placeable_cell[3][2] === "￭" ? 1 : -1,
-                placeable_cell[3][2] === "￭" && placeable_cell[4][2] === "￭" ? 2 : -1,
-                placeable_cell[2][0] === "￭" && placeable_cell[2][1] === "￭" ? 3 : -1,
-                placeable_cell[2][1] === "￭" && placeable_cell[2][3] === "￭" ? 4 : -1,
-                placeable_cell[2][3] === "￭" && placeable_cell[2][4] === "￭" ? 5 : -1,
-                placeable_cell[1][2] === "￭" && placeable_cell[2][1] === "￭" ? 6 : -1,
-                placeable_cell[1][2] === "￭" && placeable_cell[2][3] === "￭" ? 7 : -1,
-                placeable_cell[2][3] === "￭" && placeable_cell[3][2] === "￭" ? 8 : -1,
-                placeable_cell[2][1] === "￭" && placeable_cell[3][2] === "￭" ? 9 : -1,
-                placeable_cell[3][1] === "￭" && placeable_cell[3][2] === "￭" ? 10 : -1,
-                placeable_cell[1][1] === "￭" && placeable_cell[2][1] === "￭" ? 11 : -1,
-                placeable_cell[1][2] === "￭" && placeable_cell[1][3] === "￭" ? 12 : -1,
-                placeable_cell[2][3] === "￭" && placeable_cell[3][3] === "￭" ? 13 : -1,
-                placeable_cell[3][2] === "￭" && placeable_cell[3][3] === "￭" ? 14 : -1,
-                placeable_cell[2][1] === "￭" && placeable_cell[3][1] === "￭" ? 15 : -1,
-                placeable_cell[1][1] === "￭" && placeable_cell[1][2] === "￭" ? 16 : -1,
-                placeable_cell[1][3] === "￭" && placeable_cell[2][3] === "￭" ? 17 : -1,
-            ].filter(e => e !== -1);
-            //console.log([...placeable_mino].join(","));
-            console.log(`${x},${y}`);
-
-            if (placeable_mino.length > 0) {
-                const random = placeable_mino[rnd.next_int(0, placeable_mino.length)];
-                console.log(`${random}`);
-                const first_place = replace_2d_array(data[0], x, y, `${random}`);
-                const second_place = replace_2d_array(first_place, x + mino_pattern[random][0].x, y + mino_pattern[random][0].y, `${random}`);
-                const third_place = replace_2d_array(second_place, x + mino_pattern[random][1].x, y + mino_pattern[random][1].y, `${random}`);
-                return [third_place, true];
-            }
-            else {
-                return [data[0], false];
-            }
+        // 絶対もっといい方法ある
+        const placeable_mino = [
+            placeable_cell[0][2] === "￭" && placeable_cell[1][2] === "￭" ? 0 : -1,
+            placeable_cell[1][2] === "￭" && placeable_cell[3][2] === "￭" ? 1 : -1,
+            placeable_cell[3][2] === "￭" && placeable_cell[4][2] === "￭" ? 2 : -1,
+            placeable_cell[2][0] === "￭" && placeable_cell[2][1] === "￭" ? 3 : -1,
+            placeable_cell[2][1] === "￭" && placeable_cell[2][3] === "￭" ? 4 : -1,
+            placeable_cell[2][3] === "￭" && placeable_cell[2][4] === "￭" ? 5 : -1,
+            placeable_cell[1][2] === "￭" && placeable_cell[2][1] === "￭" ? 6 : -1,
+            placeable_cell[1][2] === "￭" && placeable_cell[2][3] === "￭" ? 7 : -1,
+            placeable_cell[2][3] === "￭" && placeable_cell[3][2] === "￭" ? 8 : -1,
+            placeable_cell[2][1] === "￭" && placeable_cell[3][2] === "￭" ? 9 : -1,
+            placeable_cell[3][1] === "￭" && placeable_cell[3][2] === "￭" ? 10 : -1,
+            placeable_cell[1][1] === "￭" && placeable_cell[2][1] === "￭" ? 11 : -1,
+            placeable_cell[1][2] === "￭" && placeable_cell[1][3] === "￭" ? 12 : -1,
+            placeable_cell[2][3] === "￭" && placeable_cell[3][3] === "￭" ? 13 : -1,
+            placeable_cell[3][2] === "￭" && placeable_cell[3][3] === "￭" ? 14 : -1,
+            placeable_cell[2][1] === "￭" && placeable_cell[3][1] === "￭" ? 15 : -1,
+            placeable_cell[1][1] === "￭" && placeable_cell[1][2] === "￭" ? 16 : -1,
+            placeable_cell[1][3] === "￭" && placeable_cell[2][3] === "￭" ? 17 : -1,
+        ].filter(e => e !== -1);
+        //console.log([...placeable_mino].join(","));
+        // console.log(`${x},${y}`);
+        if (placeable_mino.length > 0) {
+            const random_mino = placeable_mino[rnd.next_int(0, placeable_mino.length)];
+            // console.log(`${random_mino}`);
+            const place_cell = [
+                { x: x + mino_pattern[random_mino][0].x, y: y + mino_pattern[random_mino][0].y, },
+                { x: x + mino_pattern[random_mino][1].x, y: y + mino_pattern[random_mino][1].y, }
+            ]
+            const place_1 = replace_2d_array(board, x, y, `${random_mino}`);
+            const place_2 = replace_2d_array(place_1, place_cell[0].x, place_cell[0].y, `${random_mino}`);
+            const place_3 = replace_2d_array(place_2, place_cell[1].x, place_cell[1].y, `${random_mino}`);
+            // console.log([...place_3].map(y => y.map(x => x.length === 1 ? ` ${x}` : x)).join("\n").replace(/,/g, " "));
+            const laser_cells = data[1].filter(e => JSON.stringify(e) != JSON.stringify(random_pos) && JSON.stringify(e) != JSON.stringify(place_cell[0]) && JSON.stringify(e) != JSON.stringify(place_cell[1]));
+            return [place_3, laser_cells, data[2] + 1];
         }
         else {
-            return [data[0], false];
+            return [data[0], data[1], -1];
         }
     }
 
-    const draw_4_mino = (): string[][] => {
-        const first_place = place_random_mino([laser_drawn_board, mirror_cell[0].x, mirror_cell[0].y]);
-        const second_place = place_random_mino([first_place[0], mirror_cell[1].x, mirror_cell[1].y]);
-        const third_place = place_random_mino([second_place[0], mirror_cell[2].x, mirror_cell[2].y]);
-        const fourth_place = place_random_mino([third_place[0], mirror_cell[3].x, mirror_cell[3].y]);
-        if (first_place[1] && second_place[1] && third_place[1] && fourth_place[1]) {
-            return fourth_place[0];
+    const place_four_mino = (): string[][] => {
+        const initial: PlaceMino = [laser_drawn_board, laser_cells, 0];
+        const placed = while_f(initial, s => {
+            const result = place_random_mino(s);
+            return [result[2] != 4 && result[2] != -1, result];
+        });
+        if (![...placed[0]].flat().includes("/") && ![...placed[0]].flat().includes("\\") && placed[2] == 4) {
+            return placed[0];
         }
         else {
-            return draw_4_mino();
+            return place_four_mino();
         }
     };
 
-    console.log(draw_4_mino().map(y => y.map(x => x.length === 1 ? ` ${x}` : x)).join("\n").replace(/,/g, " "));
+    console.log(place_four_mino().map(y => y.map(x => x.length === 1 ? ` ${x}` : x)).join("\n").replace(/,/g, " "));
     //    console.log(place_mino([laser_drawn_board, mirror_cell[0].x, mirror_cell[0].y]).join("\n").replace(/,/g, " "));
     console.log("======================");
 }
